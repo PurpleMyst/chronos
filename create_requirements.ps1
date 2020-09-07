2555a4113eb4542ad929cee8db698d3b820734f2
PurpleMyst <PurpleMyst@users.noreply.github.com>
2020-09-07 19:04:31 +0200

    ci: remove obsolete dependencies from requirements.txt and do not add chronos itself to requirements.txt

--- create_requirements.ps1
+++ create_requirements.ps1
@@ -1,2 +1,7 @@
-$requirements = & py -m poetry run py -m pip freeze
+# Get the list of requirements, excluding the current project itself if present
+$requirements = (py -m poetry run py -m pip freeze).Split([Environment]::NewLine) | `
+    Where-Object { !$_.StartsWith("-e git+") }
+
+
+# Write it to requirements.txt
 [IO.File]::WriteAllLines("requirements.txt", $requirements)
